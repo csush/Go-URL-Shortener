bc39d15fd8804047cb06dfce2e982bb4744692ee
Sushant Chaudhary <csush.95@gmail.com>
2025-07-15 22:45:32 +0200

    feat: added in-memoey storage

--- storage/store.go
+++ storage/store.go
@@ -1,11 +1,20 @@
 package storage
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
-const ErrNotFoundMsg = "URL doesn't exist"
+const ErrNotFoundMsg = "url doesn't exist"
+
+type IStore interface {
+	Save(string, string)
+	Get(string) (string, error)
+}
 
 type Store struct {
 	urls map[string]string
+	mu   sync.RWMutex
 }
 
 func NewStore() *Store {
@@ -13,10 +22,14 @@ func NewStore() *Store {
 }
 
 func (s *Store) Save(shortURL, longURL string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.urls[shortURL] = longURL
 }
 
 func (s *Store) Get(shortURL string) (string, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	longURL, exists := s.urls[shortURL]
 
 	if exists {
--- storage/store_test.go
+++ storage/store_test.go
@@ -2,7 +2,7 @@ package storage
 
 import "testing"
 
-func TestStoreSave(t *testing.T) {
+func TestStore(t *testing.T) {
 	t.Run("save URL mapping", func(t *testing.T) {
 		store := &Store{urls: make(map[string]string)}
 		store.Save("http://localhost:5001/abcd", "http://csu.sh")
@@ -15,6 +15,38 @@ func TestStoreSave(t *testing.T) {
 		}
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("get URL mapping", func(t *testing.T) {
+		urls := map[string]string{
+			"http://localhost:5001/a123": "https://google.com",
+			"http://localhost:5001/321a": "http://csu.sh",
+		}
+		store := &Store{urls: urls}
+
+		got, err := store.Get("http://localhost:5001/321a")
+		want := "https://google.com"
+
+		if err != nil {
+			t.Error("shouldn't get an error but got one")
+		}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("URL mapping doesn't exist", func(t *testing.T) {
+		urls := make(map[string]string)
+		urls["http://localhost:5001/a123"] = "http://csu.sh"
+		urls["http://localhost:5001/321a"] = "https://google.com"
+		store := &Store{urls: urls}
+
+		_, err := store.Get("http://localhost:5001/doesntexist")
+
+		if err == nil {
+			t.Error("should get an error but didnt get one")
+		}
+		if err.Error() != ErrNotFoundMsg {
+			t.Errorf("should get error %q but got %q", ErrNotFoundMsg, err.Error())
 		}
 	})
 }
