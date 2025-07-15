caa0bf83e2d48d7f320ff4a6403b086d96d82849
Sushant Chaudhary <csush.95@gmail.com>
2025-07-15 22:46:46 +0200

    feat: added apis to main

--- main.go
+++ main.go
@@ -1,30 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/csush/Go-URL-Shortener/handlers"
+	"github.com/csush/Go-URL-Shortener/models"
+	"github.com/csush/Go-URL-Shortener/storage"
 )
 
-type ShortenerRequest struct {
-	Url string
-	Tag string
-}
-
 func main() {
+	store := storage.NewStore()
+	cg := models.NewCodeGenerator()
+	handler := handlers.NewHandler(store, cg)
+
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world"))
-	})
-	router.HandleFunc("POST /shorten", func(w http.ResponseWriter, r *http.Request) {
-		requestData := ShortenerRequest{}
-		err := json.NewDecoder(r.Body).Decode(&requestData)
-		if err != nil {
-			panic(err)
-		}
-		w.Write([]byte(requestData.Url))
-	})
+	router.HandleFunc("GET /", handler.RedirectURL)
+	router.HandleFunc("POST /shorten", handler.ShortenURL)
 
 	server := http.Server{
 		Addr:    ":5001",
